35cc42a42581679777a5760bf87637f4d6508708
chanwoo <humains29@gmail.com>
2021-07-03 12:48:59 +0900

    style: 코드 컨벤션 변경

--- Caculator/CalculatorReactor.swift
+++ Caculator/CalculatorReactor.swift
@@ -3,85 +3,83 @@ import ReactorKit
 import RxSwift
 
 final class CalculatorReactor: Reactor {
-    // 버튼에 따른 연산 케이스
-    enum Operation {
-        case operation((Decimal, Decimal) -> Decimal)
-        case sign((Decimal) -> Decimal)
-        case percent((Decimal) -> Decimal)
-        case result
+  // 버튼에 따른 연산 케이스
+  enum Operation {
+    case operation((Decimal, Decimal) -> Decimal)
+    case sign((Decimal) -> Decimal)
+    case percent((Decimal) -> Decimal)
+    case result
+  }
+  
+  // 뷰 버튼 인터랙션에 따른 액션
+  enum Action {
+    case inputNumber(String)
+    case inputDot(String)
+    case operation(Operation)
+    case clear
+  }
+  
+  enum Mutation {
+    case number(String)
+    case dot(String)
+    case operation(Operation)
+    case clear
+  }
+  
+  class State {
+    var displayText: String = "0"
+    var resultNum: Decimal = 0
+    var operation: Operation?
+    var inputText: String = ""
+    
+    var inputNum: Decimal {
+      guard let inputValue = Decimal(string:inputText) else {
+        return resultNum
+      }
+      return inputValue
     }
     
-    // 뷰 버튼 인터랙션에 따른 액션
-    enum Action {
-        case inputNumber(String)
-        case inputDot(String)
-        case operation(Operation)
-        case clear
+    func checkPrefixNum() {
+      if !(inputText.contains(".")) && inputText.hasPrefix("0") {
+        inputText.removeFirst()
+      }
+    }
+  }
+  
+  // State 초기화 상수 선언
+  let initialState: State = State()
+  
+  // Action -> Mutation 스트림 변환
+  func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .inputNumber(let number):
+      return Observable.just(Mutation.number(number))
+    case .inputDot(let dot):
+      return Observable.just(Mutation.dot(dot))
+    case .operation(let operation):
+      return Observable.just(Mutation.operation(operation))
+    case .clear:
+      return Observable.just(Mutation.clear)
+    }
+  }
+  
+  // 이전 Mutation 상태 받아 다음 상태 반환
+  func reduce(state: State, mutation: Mutation) -> State {
+    switch mutation {
+    case .number(let number):
+      state.inputText.append(number)
+      state.checkPrefixNum()
+      state.displayText = state.inputText
+    case .dot(let dot):
+      break
+    case .operation(let operation):
+      break
+    case .clear:
+      break
     }
     
-    enum Mutation {
-        case number(String)
-        case dot(String)
-        case operation(Operation)
-        case clear
-    }
     
-    class State {
-        var displayText: String = "0"
-        var resultNum: Decimal = 0
-        var operation: Operation?
-        var inputText: String = ""
-        
-        var inputNum: Decimal {
-            guard let inputValue = Decimal(string:inputText) else {
-                return resultNum
-            }
-            return inputValue
-        }
-        
-        func checkPrefixNum() {
-            if !(inputText.contains(".")) && inputText.hasPrefix("0") {
-                inputText.removeFirst()
-            }
-        }
-        
-        
-    }
-    
-    // State 초기화 상수 선언
-    let initialState: State = State()
-    
-    // Action -> Mutation 스트림 변환
-    func mutate(action: Action) -> Observable<Mutation> {
-        switch action {
-        case .inputNumber(let number):
-            return Observable.just(Mutation.number(number))
-        case .inputDot(let dot):
-            return Observable.just(Mutation.dot(dot))
-        case .operation(let operation):
-            return Observable.just(Mutation.operation(operation))
-        case .clear:
-            return Observable.just(Mutation.clear)
-        }
-    }
-    
-    // 이전 Mutation 상태 받아 다음 상태 반환
-    func reduce(state: State, mutation: Mutation) -> State {
-        switch mutation {
-        case .number(let number):
-            state.inputText.append(number)
-            state.checkPrefixNum()
-            state.displayText = state.inputText
-        case .dot(let dot):
-            break
-        case .operation(let operation):
-            break
-        case .clear:
-            break
-        }
-        
-        
-        return state
-    }
-    
+    return state
+  }
+  
 }
--- Caculator/CalculatorViewController.swift
+++ Caculator/CalculatorViewController.swift
@@ -5,89 +5,94 @@ import RxCocoa
 
 
 class CalculatorViewController: UIViewController, StoryboardView {
-    var disposeBag = DisposeBag()
+  var disposeBag = DisposeBag()
+  
+  @IBOutlet weak var resultLabel: UILabel!
+  
+  @IBOutlet weak var zeroButton: UIButton!
+  @IBOutlet weak var oneButton: UIButton!
+  @IBOutlet weak var twoButton: UIButton!
+  @IBOutlet weak var threeButton: UIButton!
+  @IBOutlet weak var fourButton: UIButton!
+  @IBOutlet weak var fiveButton: UIButton!
+  @IBOutlet weak var sixButton: UIButton!
+  @IBOutlet weak var sevenButton: UIButton!
+  @IBOutlet weak var eightButton: UIButton!
+  @IBOutlet weak var nineButton: UIButton!
+  @IBOutlet weak var dotButton: UIButton!
+  
+  @IBOutlet weak var acButton: UIButton!
+  
+  @IBOutlet weak var signButton: UIButton!
+  @IBOutlet weak var percentButton: UIButton!
+  @IBOutlet weak var divisionButton: UIButton!
+  @IBOutlet weak var multiplyButton: UIButton!
+  @IBOutlet weak var subtractButton: UIButton!
+  @IBOutlet weak var addButton: UIButton!
+  @IBOutlet weak var equalButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print(reactor)
+  }
+  
+  func bind(reactor: CalculatorReactor) {
+    // Action
     
-    @IBOutlet weak var resultLabel: UILabel!
+    // 숫자 및 Dot 버튼 클릭 시 인터렉션 리액터 바인딩
+    let numberButtons: [UIButton] = [zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton]
     
-    @IBOutlet weak var zeroButton: UIButton!
-    @IBOutlet weak var oneButton: UIButton!
-    @IBOutlet weak var twoButton: UIButton!
-    @IBOutlet weak var threeButton: UIButton!
-    @IBOutlet weak var fourButton: UIButton!
-    @IBOutlet weak var fiveButton: UIButton!
-    @IBOutlet weak var sixButton: UIButton!
-    @IBOutlet weak var sevenButton: UIButton!
-    @IBOutlet weak var eightButton: UIButton!
-    @IBOutlet weak var nineButton: UIButton!
-    @IBOutlet weak var dotButton: UIButton!
+    numberButtons.enumerated().forEach({ (button) in
+      button.element.rx.tap
+        .map { .inputNumber("\(button.offset)") }
+        .bind(to: reactor.action)
+        .disposed(by: disposeBag)
+    })
+    self.dotButton.rx.tap
+      .map { .inputDot(".") }
+      .bind(to:reactor.action)
+      .disposed(by: self.disposeBag)
     
-    @IBOutlet weak var acButton: UIButton!
+    // AC 버튼 클릭 시 인터렉션 리액터 바인딩
+    self.acButton.rx.tap
+      .map { .clear }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
     
-    @IBOutlet weak var signButton: UIButton!
-    @IBOutlet weak var percentButton: UIButton!
-    @IBOutlet weak var divisionButton: UIButton!
-    @IBOutlet weak var multiplyButton: UIButton!
-    @IBOutlet weak var subtractButton: UIButton!
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var equalButton: UIButton!
+    // 연산 버튼 클릭 시 인터렉션 리액터 바인딩
+    self.signButton.rx.tap
+      .map { .operation(.sign({ -$0 })) }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
+    self.percentButton.rx.tap
+      .map { .operation(.percent({ $0 / 100})) }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
+    self.divisionButton.rx.tap
+      .map { .operation(.operation({ $0 / $1 })) }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
+    self.multiplyButton.rx.tap
+      .map { .operation(.operation({ $0 * $1 })) }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
+    self.subtractButton.rx.tap
+      .map { .operation(.operation({ $0 - $1 })) }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
+    self.addButton.rx.tap
+      .map { .operation(.operation({ $0 + $1 })) }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
+    self.equalButton.rx.tap
+      .map { .operation(.result) }
+      .bind(to: reactor.action)
+      .disposed(by: self.disposeBag)
     
-    func bind(reactor: CalculatorReactor) {
-        // Action
-        
-        // 숫자 및 Dot 버튼 클릭 시 인터렉션 리액터 바인딩
-        let numberButtons: [UIButton] = [zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton]
-        
-        numberButtons.enumerated().forEach({ (button) in
-            button.element.rx.tap
-                .map { .inputNumber("\(button.offset)") }
-                .bind(to: reactor.action)
-                .disposed(by: disposeBag)
-        })
-        self.dotButton.rx.tap
-            .map { .inputDot(".") }
-            .bind(to:reactor.action)
-            .disposed(by: self.disposeBag)
-        
-        // AC 버튼 클릭 시 인터렉션 리액터 바인딩
-        self.acButton.rx.tap
-            .map { .clear }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        
-        // 연산 버튼 클릭 시 인터렉션 리액터 바인딩
-        self.signButton.rx.tap
-            .map { .operation(.sign({ -$0 })) }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        self.percentButton.rx.tap
-            .map { .operation(.percent({ $0 / 100})) }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        self.divisionButton.rx.tap
-            .map { .operation(.operation({ $0 / $1 })) }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        self.multiplyButton.rx.tap
-            .map { .operation(.operation({ $0 * $1 })) }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        self.subtractButton.rx.tap
-            .map { .operation(.operation({ $0 - $1 })) }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        self.addButton.rx.tap
-            .map { .operation(.operation({ $0 + $1 })) }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        self.equalButton.rx.tap
-            .map { .operation(.result) }
-            .bind(to: reactor.action)
-            .disposed(by: self.disposeBag)
-        
-        // State
-        // 연산 결과 뷰 바인딩
-        reactor.state.map { $0.displayText }
-            .bind(to: self.resultLabel.rx.text)
-            .disposed(by: self.disposeBag)
-    }
+    // State
+    // 연산 결과 뷰 바인딩
+    reactor.state.map { $0.displayText }
+      .bind(to: self.resultLabel.rx.text)
+      .disposed(by: self.disposeBag)
+  }
 }
